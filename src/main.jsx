c2895cfb23dc2ff0076811702a7e25281e268b6a
Gabriel Araujo Gerbi <geral@discoveryassessoria.com.br>
2026-02-02 17:58:02 -0300

    finalizado

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import PaixaoDeCristo from './paixaodecristo';
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PaixaoDeCristo />
+  </React.StrictMode>
+);
